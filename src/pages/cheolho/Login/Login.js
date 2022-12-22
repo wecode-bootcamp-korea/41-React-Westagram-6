9a84a812c0ed506c067a72babf52b6d0cd1b7263
gimcheolho <sm708311@gmail.com>
2022-12-22 15:53:04 +0900

    Add: Mission1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/cheolho/Login/Login.js
+++ src/pages/cheolho/Login/Login.js
@@ -1,7 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import '../../../styles/common.scss';
+import '../../../styles/reset.scss';
+import './Login.scss';
 
-function LoginCheolho() {
-  return <h1>반가워요!</h1>;
-}
+//jsx
+const Login = () => {
+  const navigate = useNavigate();
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
 
-export default LoginCheolho;
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  function handleInput(event) {
+    setId(event.target.value);
+  }
+
+  function handlePwInput(event) {
+    setPw(event.target.value);
+    console.log('pw', pw);
+  }
+
+  return (
+    <div className="Login">
+      <meta charSet="UTF-8" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial
+            -scale=1.0"
+      />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Document</title>
+      <div className="wrap">
+        <div className="container">
+          <div className="inner">
+            {/* header */}
+            <header className="header">
+              <h1>
+                <span className="header_tit">로고</span>
+                <a href="#" className="page_move">
+                  <img src="images/cheolho/1600px-Instagram_logo.svg.png" />
+                </a>
+              </h1>
+            </header>
+            {/* //header */}
+
+            {/* form */}
+            <form className="form">
+              <div className="input_box">
+                <input
+                  id="id"
+                  type="text"
+                  placeholder="전화번호, 사용자 이름 또는 이메일"
+                  onChange={handleInput}
+                />
+              </div>
+              <div className="input_box">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="비밀번호"
+                  onChange={handlePwInput}
+                />
+              </div>
+              <div className="button_box">
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={goToMain}
+                  disabled=""
+                >
+                  <span>로그인</span>
+                </button>
+              </div>
+            </form>
+            {/* //form */}
+            <div className="password_forget">
+              <a href className="page_move">
+                <span>비밀번호를 잊으셨나요?</span>
+                {
+                  //<Link to="/main">로그인</Link>}
+                  /*<button className="loginBtn" onClick={goToMain}>
+                      로그인
+                    </button>*/
+                }
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
