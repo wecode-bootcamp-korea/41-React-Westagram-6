55327933afda8bfaee6032b9683147cf020f20d2
jihwan <jhjhjh01000@gmail.com>
2022-12-25 17:39:06 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jihwan/Login/Login.js
+++ src/pages/jihwan/Login/Login.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 function LoginJihwan() {
+  const navigate = useNavigate();
+  const toMain = () => {
+    navigate('/main-jihwan');
+  };
+
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const saveUserId = e => {
+    setId(e.target.value);
+    console.log(e.target.value);
+  };
+  const saveUserPw = e => {
+    setPw(e.target.value);
+    console.log(e.target.value);
+  };
+  //const isValid = id.includes('@' && '.') && pw.length > 4 ? false : true;
+
+  //const activeBtn = isValid === false ? 'activeBtn' : 'loginBtn';
+
   return (
-    <div class="loginContainer">
+    <div className="login">
       <div className="loginWrap">
         <header>
           <h1>Westagram</h1>
         </header>
-        <div class="inputWrap">
+        <div className="inputWrap">
           <input
-            class="loginId"
+            className="loginId"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={saveUserId}
           />
-          <input class="loginPw" type="password" placeholder="비밀번호" />
-          <button class="loginBtn">로그인</button>
+          <input
+            className="loginPw"
+            type="password"
+            placeholder="비밀번호"
+            onChange={saveUserPw}
+          />
+          <button className="loginBtn" onClick={toMain}>
+            로그인
+          </button>
         </div>
-        <div class="forgotPw">
+        <div className="forgotPw">
           <a href="#">비밀번호를 잊으셨나요?</a>
         </div>
       </div>
